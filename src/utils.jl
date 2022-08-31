2a96050fb60b09227662c63184cc3cef2e91131c
Renato Zimmermann <renatomatz@gmail.com>
2022-08-31 13:36:08 -0400

    added utils for remote execution

--- src/utils.jl
+++ src/utils.jl
@@ -1,3 +1,5 @@
+using Distributed
+
 function mpiexec(f::Function, comm::Dict{AbstractComm})
     p = n_procs()
     fut = Vector{Future}(undef, p)
@@ -9,4 +11,24 @@ end
 
 function make_comm_mat(::Type{T}, N::Integer, p::Int64)
     reshape([RemoteChannel(()->Channel{T}(N)) for _ in 1:(p^2)], (p,p))
+end
+
+function asgn_expr(name, val)
+    Expr(:(=), Symbol(name), Symbol(val))
+end
+
+function remote_exec(expr, p::Integer, mod::Module=Main)
+    @spawnat(p, Core.eval(mod, expr))
+end
+
+function remote_asgn(name, val, p::Integer, mod::Module=Main)
+    remote_exec(asgn_expr(name, val), p, mod)
+end
+
+function channel_lambda(name, ::Type{T}, N::Integer, mod::Module=Main)
+    ()->Core.eval(mod, asgn_expr(name, Channel{T}(N)))
+end
+
+function (RemoteChannel)(name, p, ::Type{T}, N::Integer, mod::Module=Main)
+    RemoteChannel(channel_lambda(name, T, N, mod), p)
 end
