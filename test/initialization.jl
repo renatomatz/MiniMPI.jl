475433a79d4f8b03150accbf48ab29fe26336195
Renato Zimmerman <renatomatz@gmail.com>
2022-09-04 13:26:31 -0400

    added to initialization tests, all passing

--- test/initialization.jl
+++ test/initialization.jl
@@ -2,46 +2,63 @@ using Test
 using Distributed
 using MiniMPI
 
-COMM = CommDict()
-COMM[:base] = BaseComm(1)
+@testset "sdf" begin
 
-MiniMPI.init_comm()
+    @testset "Single Process" begin
 
-ch = COMM[:base]
+        @everywhere begin
+            COMM = CommDict()
+            COMM[:base] = BaseComm(1)
+        end
 
-put!(ch.och[1], 1)
-ret = take!(ch.ich[1])
+        MiniMPI.init_comm()
 
-addprocs(1)
+        ch = COMM[:base]
 
-@everywhere begin
-    using Pkg
-    Pkg.activate(".")
-    using MiniMPI
+        put!(ch.och[1], 1)
+        ret = take!(ch.ich[1])
+        @test ret == 1
 
-@everywhere begin
-    COMM = CommDict()
-    COMM[:base] = BaseComm(1)
+    end
+
+    @testset "Multiple Processes" begin
+
+        addprocs(1)
+
+        @everywhere begin
+            using Pkg
+            Pkg.activate(".")
+            using MiniMPI
+        end
+
+        @everywhere begin
+            COMM = CommDict()
+            COMM[:base] = BaseComm(1)
+        end
+
+        MiniMPI.init_comm()
+
+        @everywhere ch = COMM[:base]
+
+        # TODO: Create testing utils for expressions like this
+        all_procs = ones(Int64, nprocs())
+        if nprocs() > 1
+            all_procs[2:end] = workers()
+        end
+
+        for i in all_procs
+            for j in all_procs
+                MiniMPI.remote_return(:(put!(ch.och[$j], $i)), i)
+            end
+        end
+
+        for i in all_procs
+            for j in all_procs
+                ret = MiniMPI.remote_return(:(take!(ch.ich[$j])), i)
+                @test ret == j
+            end
+        end
+
+    end
+
 end
-
-MiniMPI.init_comm()
-
-ch = COMM[:base]
-
-# TODO: Create testing utils for expressions like this
-all_procs = ones(Int64, nprocs())
-if nprocs() > 1
-    all_procs[2:end] = workers()
-end
-
-for i in all_procs
-    MiniMPI.remote_return(:(put!(ch.och, $i)), i)
-end
-
-for i in all_procs
-    ret = MiniMPI.remote_return(:(take(ch.ich)), i)
-end
-
-for i in workers()
-
-println("DONE")
